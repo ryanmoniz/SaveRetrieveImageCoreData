9313ec518a3de12c9067a3d38f0249aa046f4b5b
Ryan Moniz <ryanm@ca.ibm.com>
2016-08-10 17:01:34 -0400

    Initial Commit

---
+++ SaveRetrieveImageCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveRetrieveImageCoreData
+//
+//  Created by Ryan Moniz on 2016-08-10.
+//  Copyright © 2016 Ryan Moniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
